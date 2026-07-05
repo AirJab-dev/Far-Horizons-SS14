4488071d7f6e2b1f54b39a9b2c24b0304b546cc0
Rain-enjoyer23 <rainenjoyer273@gmail.com>
2026-07-05 19:43:27 +0200

    another fix

--- Content.Client/Paper/UI/PaperWindow.xaml.cs
+++ Content.Client/Paper/UI/PaperWindow.xaml.cs
@@ -12,6 +12,7 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Utility;
 using Robust.Client.UserInterface.RichText;
+using Content.Client._FarHorizons.UserInterface.RichText;
 using Robust.Shared.Input;
 using Robust.Shared.IoC;
 
