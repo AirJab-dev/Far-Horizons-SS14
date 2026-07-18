02f5b24bd09406127cf1fee90b22e17fd48a599a
princess-gurchi <princess@gurchi.love>
2026-07-18 17:06:55 +0300

    elder boss

---
+++ Content.Shared/_FarHorizons/Bosses/BossCombatComponent.cs
@@ -0,0 +1,21 @@
+namespace Content.Shared._FarHorizons.Bosses;
+
+[RegisterComponent]
+public sealed partial class BossCombatComponent : Component
+{
+    [ViewVariables] public TimeSpan NextUpdate = TimeSpan.Zero;
+    [ViewVariables] public List<EntityUid> Attacks = []; // Keeps track of all spawned attack, in case they need to be cleaned up on death
+    [ViewVariables] public Dictionary<int, TimeSpan?> Cooldowns = [];
+    [DataField] public bool Paused;
+    [DataField] public List<BossMechanic> Mechanics = [];
+    [DataField] public TimeSpan RefreshRate = TimeSpan.FromSeconds(0.5);
+}
+
+[DataDefinition]
+public sealed partial class BossMechanic
+{
+    [DataField] public List<IBossMechanicConsideration> Considerations = [];
+    [DataField] public List<IBossMechanicLogic> Logic = [];
+    [DataField] public int CooldownSeconds;
+    [DataField] public int InitialCooldown;
+}
