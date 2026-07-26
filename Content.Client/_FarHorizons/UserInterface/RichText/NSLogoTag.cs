a3013a19ee8cfc7fe7928f5adc8732d9feec30af
AirJab-dev <limepole@proton.me>
2026-07-26 16:10:04 +1200

    Update NSLogoTag.cs

--- Content.Client/_FarHorizons/UserInterface/RichText/NSLogoTag.cs
+++ Content.Client/_FarHorizons/UserInterface/RichText/NSLogoTag.cs
@@ -28,7 +28,7 @@ public sealed class NSLogoTag : IMarkupTag
 
         var icon = new TextureRect
         {
-            Texture = _resourceCache.GetTexture("Textures/_FarHorizons/Logo/NSLogo.png"),
+            Texture = _resourceCache.GetTexture("/Textures/_FarHorizons/Logo/NSLogo.png"),
             TextureScale = new Vector2(0.5f, 0.5f),
         };
 
