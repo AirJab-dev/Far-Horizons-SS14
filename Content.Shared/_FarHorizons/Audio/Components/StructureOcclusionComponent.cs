9797d1cff2eb70cf7754b3cffef718e40605f4b6
AthenaAI <AthenaTheAI@outlook.com>
2026-08-05 12:12:57 -0500

    Done-ish...?

--- Content.Shared/_FarHorizons/Audio/Components/StructureOcclusionComponent.cs
+++ Content.Shared/_FarHorizons/Audio/Components/StructureOcclusionComponent.cs
@@ -14,5 +14,12 @@ public sealed partial class StructureOcclusionComponent : Component
     /// Does the structure's occlusion still work if the object is open?
     /// </summary>
     [DataField]
-    public bool DoesOcclusionWorkWhenOpen = false;
+    public bool DoesOcclusionWorkWhenOpen = false;  
+
+    /// <summary>
+    /// An additive amount to OcclusionAmount for when a structure is welded shut.
+    /// </summary>
+    [DataField]
+    public float WeldedOcclusionModifier = 2.0f;
+
 }
