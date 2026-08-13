Happyowl93/HardLight

Mail Technician: Go! Go! Intergalactic Space Courier! Pierce Through to a Brighter Tomorrow! (#3771)
## Short description
The long awaited mail update.

## Why we need to add this
Mail technicians have always just used cargo technician clothes. Let's
change that. Also adds senior attire for them. Also gives them their own
access, and gives them and cargo techs their own lockers, and... Wow,
this really got out of hand, didn't it?

## Media (Video/Screenshots)
<img width="493" height="306" alt="image"
src="https://github.com/user-attachments/assets/2d92c65e-b529-4261-82a4-f6d247c6d109"
/>

(I fixed the Asymmetry on the senior jumpsuit after taking this
picture.)

<img width="786" height="337" alt="image"
src="https://github.com/user-attachments/assets/a23130c4-18aa-4b43-921f-f880c17be975"
/>

<img width="784" height="541" alt="image"
src="https://github.com/user-attachments/assets/52dd17bb-7ae8-45e5-af1c-79539e2e1b5d"
/>

<img width="793" height="675" alt="image"
src="https://github.com/user-attachments/assets/afe3cf63-8209-4282-9f86-c98cddaa3e0b"
/>

<img width="776" height="757" alt="image"
src="https://github.com/user-attachments/assets/0d5161c2-87b3-4072-a65b-9d35860980ad"
/>

<img width="791" height="217" alt="image"
src="https://github.com/user-attachments/assets/b4476771-69fd-4c51-a0aa-d5273bb39920"
/>

<img width="786" height="244" alt="image"
src="https://github.com/user-attachments/assets/f9dafea4-ba0d-46e4-9612-a38d0c2f3908"
/>

The golden trolley...
<img width="793" height="276" alt="image"
src="https://github.com/user-attachments/assets/c7d313ba-21b9-4abe-aba5-53b85d4db797"
/>

And something for Cargo Technicians too.
<img width="794" height="246" alt="image"
src="https://github.com/user-attachments/assets/4abbdc1b-ef31-48fd-b705-6f98ad9a04c0"
/>

Golden prizes sit beyond 100 hours for the most devoted...
<img width="303" height="166" alt="image"
src="https://github.com/user-attachments/assets/a573499a-c74d-485e-a842-a1cd42c8b253"
/>

Cargo techs also sp

Resources/Locale/en-US/_Starlight/preferences/loadout-groups.ftl
@@ -67,6 +67,15 @@ loadout-group-civilian-shoes = Shoes
 loadout-group-mime-shoes = Mime shoes
 
 # Cargo
+loadout-group-cargo-technician-appraisal-tool = Cargo Technician appraisal tool
+loadout-group-mail-technician-head = Mail Technician head
+loadout-group-mail-technician-jumpsuit = Mail Technician jumpsuit
+loadout-group-mail-technician-backpack = Mail Technician backpack
+loadout-group-mail-technician-outerclothing = Mail Technician outer clothing
+loadout-group-mail-technician-shoes = Mail Technician shoes
+loadout-group-mail-technician-id = Mail Technician ID
+loadout-group-mail-technician-bag = Mail Technician bag
+loadout-group-mail-technician-trolley = Mail Technician trolley
 loadout-group-salvage-specialist-jumpsuit = Salvage Specialist jumpsuit
 loadout-group-salvage-lead-jumpsuit = Salvage Lead jumpsuit
 loadout-group-mining-specialist-jumpsuit = Mining Specialist jumpsuit

AirJab-dev/Far-Horizons-SS14

Resources/Locale/en-US/_Starlight/preferences/loadout-groups.ftl
@@ -50,6 +50,15 @@ loadout-group-iia-shoes = Lawyer Shoes
 loadout-group-civilian-shoes = Shoes
 
 # Cargo
+loadout-group-cargo-technician-appraisal-tool = Cargo Technician Appraisal Tool
+loadout-group-mail-technician-head = Mail Technician Head
+loadout-group-mail-technician-jumpsuit = Mail Technician Jumpsuit
+loadout-group-mail-technician-backpack = Mail Technician Backpack
+loadout-group-mail-technician-outerclothing = Mail Technician Outer Clothing
+loadout-group-mail-technician-shoes = Mail Technician Shoes
+loadout-group-mail-technician-id = Mail Technician ID
+loadout-group-mail-technician-bag = Mail Technician Bag
+loadout-group-mail-technician-trolley = Mail Technician Trolley
 loadout-group-salvage-specialist-jumpsuit = Salvage Specialist Jumpsuit
 loadout-group-salvage-lead-jumpsuit = Salvage Lead Jumpsuit
 loadout-group-mining-specialist-jumpsuit = Mining Specialist Jumpsuit